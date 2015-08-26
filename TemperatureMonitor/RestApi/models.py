e0f5fd7140cd4d56fa5fc81dad0a61adf2cb5097
Ivan Marchand <marchandivan@gmail.com>
2015-08-26 08:05:10 -0400

    Add Spark core support

--- TemperatureMonitor/RestApi/models.py
+++ TemperatureMonitor/RestApi/models.py
@@ -3,10 +3,18 @@ import urllib, httplib2
 import json
 from datetime import datetime
 
+
 class Server(models.Model):
+    TYPES = (
+        ('A', 'Arduino Yun'),
+        ('S', 'Spark Core'),
+    )
     name = models.CharField(max_length=20, unique=True)
-    host = models.CharField(max_length=200)
-    port = models.IntegerField()
+    type = models.CharField(max_length=1, choices=TYPES)
+    yun_host = models.CharField(max_length=200,default=None,null=True,blank=True)
+    yun_port = models.IntegerField(default=0,null=True,blank=True)
+    spark_accessToken = models.CharField(max_length=100,default=None,null=True,blank=True)
+    spark_deviceId = models.CharField(max_length=100,default=None,null=True,blank=True)
 
     def __str__(self):              # __unicode__ on Python 2
         return self.name
@@ -22,12 +30,27 @@ class Room(models.Model):
         return self.name
 
     def getTemperatureFromServer(self):
-        url = "http://%s:%s/arduino/getTemp/F" % (self.server.host, self.server.port)
+        # Arduino Yun
+        if self.server.type == 'A':
+            url = "http://%s:%s/arduino/getTemp/F" % (self.server.yun_host, self.server.yun_port)
 
-        h = httplib2.Http(".cache")
-        resp, content = h.request(url)
+            h = httplib2.Http(".cache")
+            resp, content = h.request(url)
 
-        return json.loads(content)
+            return json.loads(content)
+        # Spark Core
+        elif self.server.type == 'S':
+            url = "https://api.particle.io/v1/devices/%s/getTempF?access_token=%s" % (self.server.spark_deviceId, self.server.spark_accessToken)
+            h = httplib2.Http(".cache")
+            resp, content = h.request(url)
+
+            # Decode Spark Reply
+            print content
+            aContentJson = json.loads(content)
+            aResult = dict()
+            aResult['temperature'], aResult['humidity'] = aContentJson['result'].split(':')
+            return aResult
+        return {'error' : "Unknown server"}
 
     def getTemperature(self, simulateRoom=False):
         aJsonDoc = dict()
@@ -41,7 +64,9 @@ class Room(models.Model):
                 aJsonDoc['temperature'] = float(aResult['temperature'])
             if 'humidity' in aResult:
                 aJsonDoc['humidity'] = float(aResult['humidity'])
-        elif simulateRoom and self.temperature:
+            if 'error' in aResult:
+                aJsonDoc['error'] = aResult['error']
+        elif self.temperature:
             aJsonDoc['temperature'] = self.temperature
 
         return aJsonDoc
